388a8b62e5a6d81fbf075c0beaa7d603caa66dd1
John Bland <johnbone23@gmail.com>
2017-09-25 23:07:30 -0400

    implement calculated tournament dates

--- src/app/general/landing/landing.component.ts
+++ src/app/general/landing/landing.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./landing.component.css']
 })
 export class LandingComponent implements OnInit {
+	private wiiDate: Date;
+	private melDate: Date;
+	private days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+	private months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
   constructor() { }
 
   ngOnInit() {
+  	var wiiPast = Math.ceil( ( new Date().getDate() - new Date( '9-24-2017' ).getDate() ) / 14 );
+  	var melPast = Math.ceil( ( new Date().getDate() - new Date( '9-17-2017' ).getDate() ) / 14 );
+  	this.wiiDate = new Date( new Date().setDate( new Date( '9-24-2017' ).getDate() + wiiPast * 14 ) );
+  	this.melDate = new Date( new Date().setDate( new Date( '9-17-2017' ).getDate() + melPast * 14 ) );
   }
 
 }
